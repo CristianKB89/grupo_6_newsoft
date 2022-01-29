54db61678866e505fe2a0d6e2973f069096d8d0d
CristianKB89 <cristianiksl@gmail.com>
2022-01-29 17:34:46 -0500

    se cambia el nombre del home por index

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/index.html'));
 }); 
 
 
