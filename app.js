4e8aefb17602f207f77b2769f8d8c00112dec708
CristianKB89 <cristianiksl@gmail.com>
2022-02-23 20:23:42 -0500

    Para heroku

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 3030;
 const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
@@ -17,6 +16,6 @@ app.post('/register-confirmation',(req,res) => {
     res.sendFile(path.join(__dirname, './views/register-confirmation.html'));
 }); 
 
-app.listen(port,() => {
-    console.log('Servidor corriendo en el puerto '+ port);
+app.listen(process.env.PORT || 3000,() => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
