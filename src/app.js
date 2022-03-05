b0b17a407b3e705ca43c2224094a51a6b21b52d3
CristianKB89 <cristianiksl@gmail.com>
2022-03-05 11:44:03 -0500

    Sprint 3 EJS

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3030;
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.sendFile(path.join(__dirname, './views/index.ejs'));
 }); 
 app.get('/register',(req,res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
