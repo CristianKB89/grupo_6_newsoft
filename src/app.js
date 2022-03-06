182d4ade5afbd575388a538db1f977c99dab4ad2
CristianKB89 <cristianiksl@gmail.com>
2022-03-06 18:15:49 -0500

    Ajuste app.js

--- src/app.js
+++ src/app.js
@@ -11,28 +11,6 @@ app.use('/', rutas);
 app.use('/users', rutas_usuarios);
 app.use('/product', rutas_productos);
 
-/*
-app.get('/productCart',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/productCart.html'));
-}); 
-app.get('/informacion',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/informacion.html'));
-}); 
-app.get('/pago',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/pago.html'));
-}); 
-app.get('/envio',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/envio.html'));
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-app.get('/recover', (req,res)=>{
-    res.sendFile(__dirname + '/views/recover.html');
-});
-app.get('/productdetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
-}); */
 
 app.listen(process.env.PORT || 3030,() => {
     console.log('Servidor corriendo en el puerto 3030');
