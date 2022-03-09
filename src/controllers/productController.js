48539a6fcd5af32d69f63881a04ce4b067018999
CristianKB89 <cristianiksl@gmail.com>
2022-03-08 20:09:18 -0500

    Ajuste de rutas de los formularios.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const controlador = {
     creacion_producto: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/products/formularioCreacion_Producto.ejs'));
+        res.render(path.resolve(__dirname, '../views/products/formularioCreacionDeProducto.ejs'));
     },
     edicion_producto: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/products/formularioEdicion_Producto.ejs'));
+        res.render(path.resolve(__dirname, '../views/products/formularioEdicionDeProducto.ejs'));
     },
 }
 
