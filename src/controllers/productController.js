21d2b40053f47d89aca08c7ef925027236ec894d
Stevenson-M <jinson.moreno@gmail.com>
2022-03-18 10:13:22 -0500

    Se realizan simplicaciones en formularios/rutas y controladores

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const controlador = {
-    creacion_producto: (req, res) => {
+    creacion: (req, res) => {
         res.render(path.resolve(__dirname, '../views/products/formularioCreacionDeProducto.ejs'));
     },
-    edicion_producto: (req, res) => {
+    edicion: (req, res) => {
         res.render(path.resolve(__dirname, '../views/products/formularioEdicionDeProducto.ejs'));
     },
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controlador = require('../controllers/productController.js');
 
-router.get('/ingresar-producto', controlador.creacion_producto);
-router.post('/ingresar-producto', controlador.creacion_producto);
-router.get('/editar-producto', controlador.edicion_producto);
-router.post('/editar-producto', controlador.edicion_producto);
+router.get('/ingresar-producto', controlador.creacion);
+router.post('/ingresar-producto', controlador.creacion);
+router.get('/editar-producto', controlador.edicion);
+router.post('/editar-producto', controlador.edicion);
 
 
 module.exports = router;
