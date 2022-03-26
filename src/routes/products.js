2d407d841149cb3a59d79cbe54d3c31b239a3af3
Stevenson-M <jinson.moreno@gmail.com>
2022-03-25 20:59:09 -0500

    Se realizan correcciones

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/img/products')
+      cb(null, path.join(__dirname,'../../public/img/products'))
     },
     filename: function (req, file, cb) {
       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
