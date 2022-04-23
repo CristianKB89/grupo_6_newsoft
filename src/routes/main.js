88158fc51e6b64cc7fa3bc3b265830f1bfe3d24d
CristianKB89 <cristianiksl@gmail.com>
2022-04-23 15:42:02 -0500

    Habilita post para login

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,7 +6,7 @@ const invitadoMiddleware = require('../middlewares/invitadoMiddleware');
 const autenticacionMiddleware = require('../middlewares/autenticacionMiddleware');
 
 router.get('/', controlador.index);
-// router.post('/', controlador.loginProcess);
+router.post('/', controlador.loginProcess);
 router.get('/login',invitadoMiddleware, controlador.login);
 router.get('/products', controlador.products);
 
