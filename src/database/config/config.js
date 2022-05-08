a93551fa67a277e3a73a1105884c5b696f9fbcb9
CristianKB89 <cristianiksl@gmail.com>
2022-05-08 15:05:53 -0500

    Heroku DB

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "root",
-    "password": '',
-    "database": "newsoft6",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
@@ -14,10 +15,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": '',
-    "database": "newsoft6",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
