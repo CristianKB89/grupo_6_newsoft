4bcb3344ef53343cc216ed2c5dafd6ca3bb04750
Stevenson-M <jinson.moreno@gmail.com>
2022-06-22 15:04:10 -0500

    Se modifican validaciones front end en todos los formularios y tambien se modifican los margenes del formulario

--- public/js/userValidations.js
+++ public/js/userValidations.js
@@ -18,11 +18,17 @@ window.addEventListener('load', function () {
     nombre?.addEventListener('blur', function () {
         let ulErrorsNombreBlur = document.querySelector('.ulErrorsNombreBlur')
         if (nombre.value == '') {
-            ulErrorsNombreBlur.innerHTML += '<li>El campo nombre no debe estar vacio</li>'
+            ulErrorsNombreBlur.innerHTML += '<li>El campo nombre no debe estar vacio</li>';
+            nombre.style.border = "1px solid red";
+            nombre.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else if (nombre.value.length < 2) {
-            ulErrorsNombreBlur.innerHTML += '<li>El campo nombre debe tener mas de dos caracteres</li>'
+            ulErrorsNombreBlur.innerHTML += '<li>El campo nombre debe tener mas de dos caracteres</li>';
+            nombre.style.border = "1px solid red";
+            nombre.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
-            ulErrorsNombreBlur.innerHTML = ''
+            ulErrorsNombreBlur.innerHTML = '';
+            nombre.style.border = "2px solid green";
+            nombre.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
     });
 
@@ -36,11 +42,17 @@ window.addEventListener('load', function () {
     apellido?.addEventListener('blur', function () {
         let ulErrorsApellidoBlur = document.querySelector('.ulErrorsApellidoBlur')
         if (apellido.value == '') {
-            ulErrorsApellidoBlur.innerHTML += '<li>El campo apellido no debe estar vacio</li>'
+            ulErrorsApellidoBlur.innerHTML += '<li>El campo apellido no debe estar vacio</li>';
+            apellido.style.border = "1px solid red";
+            apellido.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else if (apellido.value.length < 2) {
-            ulErrorsApellidoBlur.innerHTML += '<li>El campo apellido debe tener mas de dos caracteres</li>'
+            ulErrorsApellidoBlur.innerHTML += '<li>El campo apellido debe tener mas de dos caracteres</li>';
+            apellido.style.border = "1px solid red";
+            apellido.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
-            ulErrorsApellidoBlur.innerHTML = ''
+            ulErrorsApellidoBlur.innerHTML = '';
+            apellido.style.border = "2px solid green";
+            apellido.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
     });
 
@@ -54,13 +66,19 @@ window.addEventListener('load', function () {
     email.addEventListener('blur', function () {
         let ulErrorsEmailBlur = document.querySelector('.ulErrorsEmailBlur')
         if (email.value == "") {
-            ulErrorsEmailBlur.innerHTML += '<li>El campo email no debe estar vacio</li>'
+            ulErrorsEmailBlur.innerHTML += '<li>El campo email no debe estar vacio</li>';
+            email.style.border = "1px solid red";
+            email.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             let expresion = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
             if (expresion.test(email.value) == false) {
-                ulErrorsEmailBlur.innerHTML += '<li>El campo email no es valido</li>'
+                ulErrorsEmailBlur.innerHTML += '<li>El campo email no es valido</li>';
+                email.style.border = "1px solid red";
+                email.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             } else {
-                ulErrorsEmailBlur.innerHTML = ''
+                ulErrorsEmailBlur.innerHTML = '';
+                email.style.border = "2px solid green";
+                email.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             }
         }
     })
@@ -74,12 +92,18 @@ window.addEventListener('load', function () {
     telefono?.addEventListener('blur', function () {
         let ulErrorsTelefonoBlur = document.querySelector('.ulErrorsTelefonoBlur')
         if (telefono.value == "") {
-            ulErrorsTelefonoBlur.innerHTML += '<li>El campo telefono no debe estar vacio</li>'
+            ulErrorsTelefonoBlur.innerHTML += '<li>El campo telefono no debe estar vacio</li>';
+            telefono.style.border = "1px solid red";
+            telefono.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             if (!isNaN(telefono.value)) {
-                ulErrorsTelefonoBlur.innerHTML = ''
+                ulErrorsTelefonoBlur.innerHTML = '';
+                telefono.style.border = "2px solid green";
+                telefono.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             } else {
-                ulErrorsTelefonoBlur.innerHTML += '<li>El campo telefono debe ser numerico</li>'
+                ulErrorsTelefonoBlur.innerHTML += '<li>El campo telefono debe ser numerico</li>';
+                telefono.style.border = "1px solid red";
+                telefono.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             }
         }
     })
@@ -95,9 +119,13 @@ window.addEventListener('load', function () {
     direccion?.addEventListener('blur', function () {
         let ulErrorsDireccionBlur = document.querySelector('.ulErrorsDireccionBlur')
         if (direccion.value == "") {
-            ulErrorsDireccionBlur.innerHTML += '<li>El campo direccion no debe estar vacio</li>'
+            ulErrorsDireccionBlur.innerHTML += '<li>El campo direccion no debe estar vacio</li>';
+            direccion.style.border = "1px solid red";
+            direccion.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
-            ulErrorsDireccionBlur.innerHTML = ''
+            ulErrorsDireccionBlur.innerHTML = '';
+            direccion.style.border = "2px solid green";
+            direccion.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
     })
 
@@ -111,13 +139,19 @@ window.addEventListener('load', function () {
     contrasena.addEventListener('blur', function () {
         let ulErrorsContrasenaBlur = document.querySelector('.ulErrorsContrasenaBlur')
         if (contrasena.value == "") {
-            ulErrorsContrasenaBlur.innerHTML += '<li>El campo contraseña no debe estar vacio</li>'
+            ulErrorsContrasenaBlur.innerHTML += '<li>El campo contraseña no debe estar vacio</li>';
+            contrasena.style.border = "1px solid red";
+            contrasena.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             let expresion = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/
             if (expresion.test(contrasena.value) == false) {
-                ulErrorsContrasenaBlur.innerHTML += '<li>La contraseña debe tener al menos 8 caracteres, al menos una letra mayuscula, al menos una letra minuscula, al menos un numero y al menos un caracter especial</li>'
+                ulErrorsContrasenaBlur.innerHTML += '<li>La contraseña debe tener al menos 8 caracteres, al menos una letra mayuscula, al menos una letra minuscula, al menos un numero y al menos un caracter especial</li>';
+                contrasena.style.border = "1px solid red";
+                contrasena.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             } else {
-                ulErrorsContrasenaBlur.innerHTML = ''
+                ulErrorsContrasenaBlur.innerHTML = '';
+                contrasena.style.border = "2px solid green";
+                contrasena.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             }
         }
     })
@@ -141,12 +175,18 @@ window.addEventListener('load', function () {
         if (nombre.value == "") {
             errores.push('El campo nombre no debe estar vacio');
             nombre.classList.add('is-invalid');
+            nombre.style.border = "1px solid red";
+            nombre.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else if (nombre.value.length < 2) {
             errores.push('El campo nombre debe tener al menos 2 caracteres');
             nombre.classList.add('is-invalid');
+            nombre.style.border = "1px solid red";
+            nombre.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             nombre.classList.remove('is-invalid');
             nombre.classList.add('is-valid');
+            nombre.style.border = "2px solid green";
+            nombre.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
         // 
 
@@ -154,12 +194,18 @@ window.addEventListener('load', function () {
         if (apellido.value == "") {
             errores.push('El campo apellido no debe estar vacio');
             apellido.classList.add('is-invalid');
+            apellido.style.border = "1px solid red";
+            apellido.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else if (apellido.value.length < 2) {
             errores.push('El campo apellido debe tener al menos 2 caracteres');
             apellido.classList.add('is-invalid');
+            apellido.style.border = "1px solid red";
+            apellido.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             apellido.classList.remove('is-invalid');
             apellido.classList.add('is-valid');
+            apellido.style.border = "2px solid green";
+            apellido.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
         // 
 
@@ -167,14 +213,20 @@ window.addEventListener('load', function () {
         if (email.value == "") {
             errores.push('El campo email no debe estar vacio');
             email.classList.add('is-invalid');
+            email.style.border = "1px solid red";
+            email.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             let expresion = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
             if (expresion.test(email.value) == false) {
                 errores.push('El campo email no es valido');
                 email.classList.add('is-invalid');
+                email.style.border = "1px solid red";
+                email.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             } else {
                 email.classList.remove('is-invalid');
                 email.classList.add('is-valid');
+                email.style.border = "2px solid green";
+                email.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             }
         }
         // 
@@ -183,13 +235,19 @@ window.addEventListener('load', function () {
         if (telefono.value == "") {
             errores.push('El campo telefono no debe estar vacio');
             telefono.classList.add('is-invalid');
+            telefono.style.border = "1px solid red";
+            telefono.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             if (!isNaN(telefono.value)) {
                 telefono.classList.remove('is-invalid');
                 telefono.classList.add('is-valid');
+                telefono.style.border = "2px solid green";
+                telefono.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             } else {
                 errores.push('El campo telefono debe ser numerico');
                 telefono.classList.add('is-invalid');
+                telefono.style.border = "1px solid red";
+                telefono.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             }
         }
         // 
@@ -198,9 +256,13 @@ window.addEventListener('load', function () {
         if (direccion.value == "") {
             errores.push('El campo direccion no debe estar vacio');
             direccion.classList.add('is-invalid');
+            direccion.style.border = "1px solid red";
+            direccion.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             direccion.classList.remove('is-invalid');
             direccion.classList.add('is-valid');
+            direccion.style.border = "2px solid green";
+            direccion.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
         }
         // 
 
@@ -208,14 +270,20 @@ window.addEventListener('load', function () {
         if (contrasena.value == "") {
             errores.push('El campo contraseña no debe estar vacio');
             contrasena.classList.add('is-invalid');
+            contrasena.style.border = "1px solid red";
+            contrasena.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
         } else {
             let expresion = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/
             if (expresion.test(contrasena.value) == false) {
                 errores.push('La contraseña debe tener al menos 8 caracteres, al menos una letra mayuscula, al menos una letra minuscula, al menos un numero y al menos un caracter especial');
                 contrasena.classList.add('is-invalid');
+                contrasena.style.border = "1px solid red";
+                contrasena.style.backgroundColor = "rgba(220, 90, 114, 0.1)";
             } else {
                 contrasena.classList.remove('is-invalid');
                 contrasena.classList.add('is-valid');
+                contrasena.style.border = "2px solid green";
+                contrasena.style.backgroundColor = "rgba(90, 220, 108, 0.1)";
             }
         }
         //
